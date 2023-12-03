c394f7cb8f0a4a42788d3e894915b00c7f2e76d6
atsekhan <atsekhan@gmail.com>
2023-12-03 15:05:01 -0500

    Added DriveManuallyCommand

--- frankenbotDriveNEOTristan/src/main/java/frc/robot/RobotContainer.java
+++ frankenbotDriveNEOTristan/src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,11 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.DriveManuallyCommand;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -21,6 +24,9 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  public final static DriveSubsystem driveSubsystem = new DriveSubsystem();
+
+  public static Joystick driveStick;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -30,6 +36,7 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    driveSubsystem.setDefaultCommand(new DriveManuallyCommand());
   }
 
   /**
