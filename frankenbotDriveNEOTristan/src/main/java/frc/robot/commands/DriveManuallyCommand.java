f4197464c476799ef1c71e6a3e0aec19033e88f8
atsekhan <atsekhan@gmail.com>
2023-12-04 18:17:54 -0500

    Re-Commit just since I got back after a day.

--- frankenbotDriveNEOTristan/src/main/java/frc/robot/commands/DriveManuallyCommand.java
+++ frankenbotDriveNEOTristan/src/main/java/frc/robot/commands/DriveManuallyCommand.java
@@ -6,7 +6,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.DriveSubsystem;
+//import frc.robot.subsystems.DriveSubsystem;
 
 public class DriveManuallyCommand extends CommandBase {
   /** Creates a new DriveManuallyCommand. */
