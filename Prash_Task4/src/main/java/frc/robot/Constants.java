f733130f1e7be8ada4adcbe17ea70100e7253497
CoachPrash <69826040+CoachPrash@users.noreply.github.com>
2023-12-17 14:51:02 -0500

    Added pneumatics code

--- Prash_Task4/src/main/java/frc/robot/Constants.java
+++ Prash_Task4/src/main/java/frc/robot/Constants.java
@@ -15,5 +15,10 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int JOYSTICK_ID = 0;
+    public static final int BUTTON_ID = 11;
+    public static final int COMPRESSOR_ID = 0;
+    public static final int FORWARD_CHANNEL = 0;
+    public static final int REVERSE_CHANNEL = 7;    
   }
 }
