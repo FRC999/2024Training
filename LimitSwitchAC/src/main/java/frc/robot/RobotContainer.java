feca37ac0e83129ed78a957ced23d99c47512670
atsekhan <atsekhan@gmail.com>
2023-12-18 20:42:39 -0500

    Finished

--- LimitSwitchAC/src/main/java/frc/robot/RobotContainer.java
+++ LimitSwitchAC/src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,9 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.NavXSubsystem;
+import frc.robot.subsystems.PigeonSubsystem;
+import frc.robot.subsystems.SmartDashboardSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -19,8 +22,11 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  public final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  public static final PigeonSubsystem pigeonSubsystem = new PigeonSubsystem();
+  public static final NavXSubsystem navXSubsystem = new NavXSubsystem();
+  public static final SmartDashboardSubsystem smartDashboardSubsystem = new SmartDashboardSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
--- LimitSwitchAC/src/main/java/frc/robot/subsystems/SmartDashboardSubsystem.java
+++ LimitSwitchAC/src/main/java/frc/robot/subsystems/SmartDashboardSubsystem.java
@@ -4,23 +4,29 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
 
 public class SmartDashboardSubsystem extends SubsystemBase {
   /** Creates a new SmartDashboardSubsystem. */
   public SmartDashboardSubsystem() {}
 
-  public void updatePigeon() {
-
+  public void updatePigeonIMUValues() {
+    SmartDashboard.putNumber("Pigeon Roll: ", RobotContainer.pigeonSubsystem.getRoll());
+    SmartDashboard.putNumber("Pigeon Pitch: ", RobotContainer.pigeonSubsystem.getPitch());
+    SmartDashboard.putNumber("Pigeon Yaw: ", RobotContainer.pigeonSubsystem.getYaw());
   }
-  
-  public void updateNavX(){
 
-  }
-  
+public void updateNavXIMUValues(){
+  SmartDashboard.putNumber("NavX Roll: ", RobotContainer.navXSubsystem.getRoll());
+  SmartDashboard.putNumber("NavX Pitch: ", RobotContainer.navXSubsystem.getPitch());
+  SmartDashboard.putNumber("NavX Yaw: ", RobotContainer.navXSubsystem.getYaw());
+}
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    updatePigeonIMUValues();
+    updateNavXIMUValues();
   }
 }
