c8b861e98c078212bd0c836af252f1502107d0fd
atsekhan <atsekhan@gmail.com>
2023-12-22 20:30:17 -0500

    Exercise 6 completed and tested.

--- Task6AH/src/main/java/frc/robot/commands/TurnMotor.java
+++ Task6AH/src/main/java/frc/robot/commands/TurnMotor.java
@@ -6,27 +6,23 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
-//import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
 
-public class DriveManuallyCommand extends CommandBase {
-  /** Creates a new DriveManuallyCommand. */
-  public DriveManuallyCommand() {
-    addRequirements(RobotContainer.driveSubsystem);
+public class TurnMotor extends CommandBase {
+  /** Creates a new TurnMotor. */
+  public TurnMotor() {
+     addRequirements(RobotContainer.driveSubsystem); 
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    RobotContainer.driveSubsystem.driveTrainBrakeMode();
+    RobotContainer.driveSubsystem.startMotor(); 
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    double move = RobotContainer.driveStick.getX();
-    double turn = RobotContainer.driveStick.getY()*(-1);
-    RobotContainer.driveSubsystem.manualDrive(move, turn);
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
